fdbf2af886e0798a814d5594d3262f79211ded5f
Shashwat Swaraj <shashwat_swaraj@epam.com>
2025-04-04 17:44:14 +0530

    2

--- task06/modules/webapp/variables.tf
+++ task06/modules/webapp/variables.tf
@@ -1,35 +1,39 @@
 variable "resource_group_name" {
   type        = string
-  description = "Resource group name"
+  description = "Name of the resource group where resources will be deployed"
 }
 
 variable "service_plan_name" {
   type        = string
-  description = "App Service Plan name"
+  description = "Name of the App Service Plan"
 }
 
 variable "webapp_name" {
   type        = string
-  description = "Web Application name"
+  description = "Name of the Linux Web Application"
 }
 
 variable "location" {
   type        = string
-  description = "Azure region"
+  description = "Azure region where resources will be deployed"
 }
 
 variable "dotnet_version" {
   type        = string
-  description = ".NET version"
+  description = ".NET version for the web application stack"
+  default     = "8.0"
 }
 
 variable "sql_connection_string" {
   type        = string
-  description = "SQL database connection string"
+  description = "Sensitive SQL database connection string"
   sensitive   = true
 }
 
 variable "tags" {
   type        = map(string)
-  description = "Resource tags"
+  description = "Resource tags to apply to all components"
+  default = {
+    Creator = "shashwat_swaraj@epam.com"
+  }
 }
